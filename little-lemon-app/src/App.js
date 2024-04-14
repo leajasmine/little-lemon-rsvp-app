38eccd945cfe963524e387bd0c1d32f919408e2e
Lea Jasmine <sunshinemintlime@gmail.com>
2024-04-14 08:26:09 -0400

    created Header, Nav, Main, Footer files and added content for them on App.js file

--- little-lemon-app/src/App.js
+++ little-lemon-app/src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import './Header.js';
+import './Nav.js';
+import './Main.js';
+import './Footer.js';
 
 function App() {
-  <div>
-    <h1>Homepage</h1>
-  </div>;
+  <>
+  <Nav>
+    <img src="logo.svg" alt="logo" />
+    <ul>
+      <li><a href="/">Homepage</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/menu">Menu</a></li>
+      <li><a href="/reservations">Reservations</a></li>
+      <li><a href="/order">Order Online</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+  </Nav>
+  <Header>
+  </Header>
+  <Main>
+      <p>Welcome to the homepage!</p>
+  </Main>
+  <Footer>
+    <ul>
+      <li><a href="/">Homepage</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/menu">Menu</a></li>
+      <li><a href="/reservations">Reservations</a></li>
+      <li><a href="/order">Order Online</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+
+    <p>Contact</p>
+    <p>Address</p>
+    <p>Phone</p>
+    <p>Email</p>
+
+    <p>Social Media Links</p>
+    <ul>
+      <li><a href="https://www.facebook.com">Facebook</a></li>
+      <li><a href="https://www.twitter.com">Twitter</a></li>
+      <li><a href="https://www.instagram.com">Instagram</a></li>
+    </ul>
+  </Footer></>
+
 }
 
 export default App;
